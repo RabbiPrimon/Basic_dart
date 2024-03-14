69f076b31dd7b0398509a8468cf0f5947a0019b7
User <rabbiprimon00000@gmail.com>
2024-03-15 00:08:05 +0600

    Completed assignment

---
+++ assignment_01.dart
@@ -0,0 +1,25 @@
+class Media {
+  void play()
+  {
+    print('playing media..');
+  }
+}
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+  @override
+  void play() {
+    print('playing song by $artist');
+  }
+}
+
+void main()
+{
+  Media media = Media();
+  Song song = Song('Weekend') ;
+  media.play();
+  song.play();
+  //
+}
